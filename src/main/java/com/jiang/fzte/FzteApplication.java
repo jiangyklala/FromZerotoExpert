4720b3f773a8cd3ac57041c787123152ec8691eb
Jxm <969904729@qq.com>
2022-09-06 12:08:44 +0800

    使用 SpringBoot 构建项目

---
+++ src/main/java/com/jiang/fzte/FzteApplication.java
@@ -0,0 +1,13 @@
+package com.jiang.fzte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FzteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FzteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiang/fzte/FzteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiang.fzte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FzteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
